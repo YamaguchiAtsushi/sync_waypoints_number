0476fbf4fded4027a008cd4e29224d5a3f31968c
YamaguchiAtsushi <yamaguchi.atsushi2000@gmail.com>
2024-08-02 17:31:03 +0900

    one robot waypoint move using sync_waypoint_number

--- src/sync_waypoints_number.cpp
+++ src/sync_waypoints_number.cpp
@@ -22,13 +22,16 @@ std::map<std::string, double> params_; // パラメータをここに格納
 //std_msgs::Int16 waypoints_A_number_now; // 今向かっているロボットAのwaypointの番号;
 std_msgs::Int16 waypoints_A_number_next; // 送られてくる次に向かうロボットAのwaypointの番号
 
-int waypoints_A_number_now = 0; //今向かっているロボットAのwaypointの番号
+int waypoints_A_number_now = -1; //今向かっているロボットAのwaypointの番号
 //int waypoints_A_number_next = 0; // 送られてくる次に向かうロボットAのwaypointの番号
-
+bool send_flag = false;
+int before = 100;
 
 void numberCallback(const std_msgs::Int16::ConstPtr &msg)
 {
+
     waypoints_A_number_now = msg->data;
+
 }
 
 int main(int argc, char **argv)
@@ -39,24 +42,22 @@ int main(int argc, char **argv)
 	ros::NodeHandle pnh("~");
 
 	// Subscriber, Publisherの定義
-    ros::Subscriber waypoints_A_number_sub = nh.subscribe("waypoints_A_number", 10, numberCallback);
-    ros::Publisher waypoints_A_number_pub = nh.advertise<std_msgs::Int16>("waypoints_A_number", 10);
+    ros::Subscriber waypoints_A_number_sub = nh.subscribe("waypoints_A_number_now", 10, numberCallback);
+    ros::Publisher waypoints_A_number_pub = nh.advertise<std_msgs::Int16>("waypoints_A_number_next", 10);
 
 	ros::Rate loop_rate(100);
 
 
-    int to_be_waypoints_number = 0;
-
     while(ros::ok()){
+        
+
         ros::spinOnce();
-        if (waypoints_A_number_now == to_be_waypoints_number){
-            waypoints_A_number_next.data = waypoints_A_number_now + 1;
-            waypoints_A_number_pub.publish(waypoints_A_number_next);
-            to_be_waypoints_number +=1;
-        }
 
+        waypoints_A_number_next.data = waypoints_A_number_now + 1;
+        waypoints_A_number_pub.publish(waypoints_A_number_next);
+        std::cout << "waypoints_A_number_now: " << waypoints_A_number_now << std::endl;
+        //std::cout << "waypoints_A_number_next: " << waypoints_A_number_next.data << std::endl;
 
-        std::cout << waypoints_A_number_now << std::endl;
 
         loop_rate.sleep();
     }
