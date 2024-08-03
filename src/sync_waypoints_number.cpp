8ed9e09a523701aed8aa0dbc50f97075e4ba0206
YamaguchiAtsushi <yamaguchi.atsushi2000@gmail.com>
2024-08-03 16:38:12 +0900

    change code to be able to sync robots

--- src/sync_waypoints_number.cpp
+++ src/sync_waypoints_number.cpp
@@ -21,16 +21,27 @@
 std::map<std::string, double> params_; // パラメータをここに格納
 //std_msgs::Int16 waypoints_A_number_now; // 今向かっているロボットAのwaypointの番号;
 std_msgs::Int16 waypoints_A_number_next; // 送られてくる次に向かうロボットAのwaypointの番号
+std_msgs::Int16 waypoints_B_number_next; // 送られてくる次に向かうロボットAのwaypointの番号
+
 
 int waypoints_A_number_now = -1; //今向かっているロボットAのwaypointの番号
+int waypoints_B_number_now = -1; //今向かっているロボットAのwaypointの番号
+
 //int waypoints_A_number_next = 0; // 送られてくる次に向かうロボットAのwaypointの番号
 bool send_flag = false;
 int before = 100;
 
-void numberCallback(const std_msgs::Int16::ConstPtr &msg)
+void A_numberCallback(const std_msgs::Int16::ConstPtr &msg)
 {
 
     waypoints_A_number_now = msg->data;
+
+}
+
+void B_numberCallback(const std_msgs::Int16::ConstPtr &msg)
+{
+
+    waypoints_B_number_now = msg->data;
 
 }
 
@@ -42,8 +53,10 @@ int main(int argc, char **argv)
 	ros::NodeHandle pnh("~");
 
 	// Subscriber, Publisherの定義
-    ros::Subscriber waypoints_A_number_sub = nh.subscribe("robotA/waypoints_A_number_now", 10, numberCallback);
+    ros::Subscriber waypoints_A_number_sub = nh.subscribe("robotA/waypoints_A_number_now", 10, A_numberCallback);
     ros::Publisher waypoints_A_number_pub = nh.advertise<std_msgs::Int16>("robotA/waypoints_A_number_next", 10);
+    ros::Subscriber waypoints_B_number_sub = nh.subscribe("robotB/waypoints_A_number_now", 10, B_numberCallback);
+    ros::Publisher waypoints_B_number_pub = nh.advertise<std_msgs::Int16>("robotB/waypoints_A_number_next", 10);
 
 	ros::Rate loop_rate(100);
 
@@ -53,10 +66,15 @@ int main(int argc, char **argv)
 
         ros::spinOnce();
 
-        waypoints_A_number_next.data = waypoints_A_number_now + 1;
-        waypoints_A_number_pub.publish(waypoints_A_number_next);
         std::cout << "waypoints_A_number_now: " << waypoints_A_number_now << std::endl;
         //std::cout << "waypoints_A_number_next: " << waypoints_A_number_next.data << std::endl;
+
+        if (waypoints_A_number_now == waypoints_B_number_now){
+            waypoints_A_number_next.data = waypoints_A_number_now + 1;
+            waypoints_B_number_next.data = waypoints_B_number_now + 1;
+        }
+        waypoints_A_number_pub.publish(waypoints_A_number_next);
+        waypoints_B_number_pub.publish(waypoints_B_number_next);
 
 
         loop_rate.sleep();
